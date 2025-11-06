0f9969b7a6abea27383b88a9f675c44e772ee519
elsen111 <csstudent1111@gmail.com>
2025-11-06 03:36:26 +0300

    Creating React+Vite Boilerplate. Edited only for testing

---
+++ news-feed-app/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <p className='bg-black text-white h-[100px] w-[200px] rounded-2xl text-center'>Hello everyone</p>
+      </div>
+    </>
+  )
+}
+
+export default App
