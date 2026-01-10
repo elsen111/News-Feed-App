71b5b813ee8a95cda25751ce0ca3308ecd873bc7
elsen111 <csstudent1111@gmail.com>
2026-01-10 17:45:16 +0300

    Tooltip in news cards was updated

--- news-feed-app/src/components/_shared/NewsCard/ToolTip.jsx
+++ news-feed-app/src/components/_shared/NewsCard/ToolTip.jsx
@@ -5,10 +5,10 @@ const ToolTip = ({ link }) => {
     <a
       href={link}
       target="_blank"
-      className="cursor-pointer flex gap-1 justify-center bg-emerald-950/80 w-full h-full! items-center text-white text-[20px]"
+      className="cursor-pointer flex gap-1 justify-center bg-emerald-950/80 w-full h-full! px-4 py-1 rounded-md items-center text-white text-[14px] hover:bg-emerald-950 group transition-all duration-300"
     >
       <span className="group-hover:scale-100 scale-50 transition-all duration-300">Read more</span>
-      <span className="opacity-0 duration-1000 group-hover:translate-x-5 group-hover:opacity-100">
+      <span className="opacity-0 duration-1000 group-hover:translate-x-2 group-hover:opacity-100">
         <FaArrowRight />
       </span>
     </a>
--- news-feed-app/src/components/_shared/NewsCard/index.jsx
+++ news-feed-app/src/components/_shared/NewsCard/index.jsx
@@ -80,7 +80,7 @@ const NewsCard = ({
   );
 
   return (
-    <div className="rounded-t-[10px] overflow-hidden shadow-md transition duration-300 hover:-translate-y-2.5 hover:shadow-2xl">
+    <div className="rounded-t-[10px] overflow-hidden shadow-md transition duration-300 hover:-translate-y-2.5 hover:shadow-2xl pb-5">
       <div className="relative w-full h-auto sm:h-[380px] md:h-[200px]">
         <img src={image_url || newsImg} onError={handleImageError} className="h-full w-full" />
         <span className="py-1.5 px-2.5 bg-[#147d83] uppercase text-white absolute bottom-0 rounded-tr-[10px] text-[12px] font-bold tracking-[0.5px]">
@@ -94,8 +94,8 @@ const NewsCard = ({
           )}
         </div>
       </div>
-      <div className="group p-[15px] flex flex-col gap-y-[18px] justify-center relative">
-        <div className="opacity-0 flex absolute left-0 h-full w-full transition-all duration-300 md:group-hover:opacity-100">
+      <div className="group p-[15px] flex flex-col gap-y-[18px] justify-center h-auto">
+        <div className="opacity-0 flex absolute bottom-1 left-1 right-1 rounded-2xl justify-center items-center transition-all duration-300 md:group-hover:opacity-100">
           <ToolTip link={link} />
         </div>
         <h6 className="cursor-pointer">
